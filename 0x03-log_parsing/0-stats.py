c27c014961ad0e9d1cb49fb218230dd5751aef3d
AturiheihiBlendon <2020blendon@gmail.com>
2023-02-15 08:56:29 +0300

    read line by line and compute the metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,7 +6,6 @@ reads stdin line by line and computes metrix
 import sys
 
 file_size = 0
-code = 0
 counter = 0
 s_codes = {
     "200": 0,
@@ -34,14 +33,12 @@ try:
     for line in sys.stdin:
         line_pas = line.split(" ")
         if len(line_pas) > 4:
-            status_code = line_pas[-2]
+            code = line_pas[-2]
             filesize = int(line_pas[-1])
+            file_size += filesize
+            counter += 1
             if code in s_codes.keys():
                 s_codes[code] += 1
-            else:
-                file_size += filesize
-                counter += 1
-
         if counter == 10:
             counter = 0
             print_stats(s_codes, file_size)
